4ed84dddcf7d47a1555e390be05cdad1f864a906
vmeet95 <91734696+vmeet95@users.noreply.github.com>
2021-10-01 17:55:42 +0530

    Update table.c

--- c/table.c
+++ c/table.c
@@ -1,17 +1,21 @@
 #include<stdio.h>
 #include<conio.h>
-void main(){
+int main()
+{
     int n,i;
+    
     n = 2;
+    
     while (n<=20)
     {
-    i=1;
-    printf("\n");
-    while (i<=10)
-    {
-        printf("%5d", n*i);
-        i++;
-    }
-    n++;
+    	i=1;
+    	printf("\n");
+    	
+		while (i<=10)
+    	{
+     	   printf("%5d", n*i);
+     	   i++;
+   	 	}
+   	 	n++;
     }
 }
