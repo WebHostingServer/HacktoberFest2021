24df1e23fe1863dee3147c56df696297cdc22268
Awate Akshay <86955808+AwateAkshay@users.noreply.github.com>
2021-10-12 14:16:10 +0530

    Added webserver in go

---
+++ go/webserver.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/name", showName)
+
+	http.ListenAndServe(":8989", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from Go web server")
+}
+func showName(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from Go web server akshay")
+}
